931aaa67619f2c0d459237adf9f73e496cf28cc7
xmatusk3 <445561@mail.muni.cz>
2019-04-28 23:34:50 +0200

    add delete session button

--- src/components/AppContainer/Header/Header.tsx
+++ src/components/AppContainer/Header/Header.tsx
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { HeaderProps } from "./types";
 import "./Header.css";
 
-const Header: React.FC<HeaderProps> = props => {
-  return (
-    <div className="header">
-      <div className="header-text">
-        <h1>TASK</h1>
-        <h1>BASK</h1>
+class Header extends Component<HeaderProps> {
+  render() {
+    return (
+      <div className="header">
+        <div className="header-text">
+          <h1>TASK</h1>
+          <h1>BASK</h1>
+        </div>
+        <div className="header-actions">
+          <i
+            className="fas fa-cog"
+            title="Edit session"
+            onClick={this.props.onEditClick}
+          />
+          <i className="fas fa-times" title="Delete session" />
+        </div>
       </div>
-      <div className="header-edit">
-        <i
-          className="fas fa-cog"
-          title="Edit session"
-          onClick={props.onEditClick}
-        />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
