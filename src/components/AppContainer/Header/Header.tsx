f5423fc8da8e2b3deb252830606244457a4b34b2
xmatusk3 <445561@mail.muni.cz>
2019-04-28 21:56:26 +0200

    change Header to a functional component

--- src/components/AppContainer/Header/Header.tsx
+++ src/components/AppContainer/Header/Header.tsx
@@ -1,30 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./Header.css";
 import { HeaderProps } from "./types";
 
-class Header extends Component<HeaderProps> {
-  constructor(props: any) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="header">
-        <div className="header-text">
-          <h1>TASK</h1>
-          <h1>BASK</h1>
-        </div>
-        <div className="header-edit">
-          <i
-            className="fas fa-cog"
-            title="Edit session"
-            onClick={this.props.onEditClick}
-          />
-        </div>
+const Header: React.FC<HeaderProps> = props => {
+  return (
+    <div className="header">
+      <div className="header-text">
+        <h1>TASK</h1>
+        <h1>BASK</h1>
       </div>
-    );
-  }
-}
+      <div className="header-edit">
+        <i
+          className="fas fa-cog"
+          title="Edit session"
+          onClick={props.onEditClick}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
